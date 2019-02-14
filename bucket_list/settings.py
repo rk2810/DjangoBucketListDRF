79d0fc889ef62c4c3a775607791a2a4c42380b33
rk2810 <rohan.kumquat@gmail.com>
2019-02-14 14:49:37 +0530

    More changes for heroku.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

--- bucket_list/settings.py
+++ bucket_list/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': os.env.get('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
